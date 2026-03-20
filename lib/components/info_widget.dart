4c0fdb7b9a86bac7395838127369ec0fd080de8c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-20 02:15:18 +0000

    componente info

---
+++ lib/components/info_widget.dart
@@ -0,0 +1,39 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'info_model.dart';
+export 'info_model.dart';
+
+class InfoWidget extends StatefulWidget {
+  const InfoWidget({super.key});
+
+  @override
+  State<InfoWidget> createState() => _InfoWidgetState();
+}
+
+class _InfoWidgetState extends State<InfoWidget> {
+  late InfoModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => InfoModel());
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
